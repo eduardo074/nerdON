e23a61e5e5a451959ad914f9392003504e32fe10
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-08-11 14:22:37 -0300

    add menu hamburge

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let count = 1;
+const btnMobile =document.getElementById('btn-mobile');
+
 document.getElementById("radio1").checked = true;
 
 setInterval( function(){
@@ -13,3 +15,19 @@ function nextImage(){
 
     document.getElementById("radio"+count).checked = true;
 }
+
+function toggleMenu(event){
+    if (event.type == 'touchstart') event.preventDefault();
+        const nav =document.getElementById('nav');
+        nav.classList.toggle('active'); 
+        const active = nav.classList.contains('active');
+        event.currentTarget.setAttribute('arial-expended', active);
+        if(active){
+            event.currentTarget.setAttribute('aria-label' , 'Fechar menu')
+        } else{
+            event.currentTarget.setAttribute('aria-label' , 'Abrir menu')
+        }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
