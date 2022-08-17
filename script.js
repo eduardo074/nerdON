2c167ad6df8235ddc05377230f76888469dbe422
Eduardo Souza <103686965+eduardo074@users.noreply.github.com>
2022-08-17 11:13:41 -0300

    att

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 let count=1;const btnMobile=document.getElementById("btn-mobile"),setaMenu=document.getElementById("seta");document.getElementById("radio1").checked=!0,setInterval(function(){nextImage()},8e3);function nextImage(){count++,4<count&&(count=1),document.getElementById("radio"+count).checked=!0}function toggleMenu(a){"touchstart"==a.type&&a.preventDefault();const b=document.getElementById("nav");b.classList.toggle("active");const c=b.classList.contains("active");a.currentTarget.setAttribute("arial-expended",c),c?a.currentTarget.setAttribute("aria-label","Fechar menu"):a.currentTarget.setAttribute("aria-label","Abrir menu")}btnMobile.addEventListener("click",toggleMenu),setaMenu.addEventListener("click",toggleMenu),btnMobile.addEventListener("touchstart",toggleMenu);
 
+
+
+
